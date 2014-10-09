bf729ccc9230db6e30f8090a68ef189f059bf14b
casey_frederick <1deaged@gmail.com>
2014-10-09 19:51:37 -0400

    reservations model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009231803) do
+ActiveRecord::Schema.define(version: 20141009235104) do
+
+  create_table "authors", force: true do |t|
+    t.string   "name"
+    t.date     "dob"
+    t.string   "nationality"
+    t.string   "awards"
+    t.string   "biography"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "books", force: true do |t|
+    t.string   "isbn"
+    t.string   "title"
+    t.integer  "author_id"
+    t.string   "genre"
+    t.text     "abstract"
+    t.integer  "pages"
+    t.string   "image_cover_url"
+    t.date     "published_on"
+    t.integer  "total_in_library"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reservations", force: true do |t|
+    t.date     "reserved_on"
+    t.date     "due_on"
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
