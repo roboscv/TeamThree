41ff0ea01cb0af789f9f107199c8e4d653916903
Jamie Cheng <jacheng@vt.edu>
2014-10-09 21:32:20 -0400

    books and authors have data populated from seeds

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ActiveRecord::Base
+	validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, presence: true
+	validates :abstract, 
+		length: {minimum: 15, :message => "must be at least 15 characters."}
+	validates :pages, :total_in_library, 
+		numericality: { only_integer:true, greater_than: 0, :message => "must be a positive number" }
+	validates :genre,
+		inclusion: { in: %w(Fiction Non-Fiction Suspense History Autobiography Horror), :message => "is not a valid genre"}
+	#validates :author,
+	#	inclusion: {in: %w(Stephen King, Dan Brown, Oprah Winfrey), :message => "is not a valid uthor"}
 end
