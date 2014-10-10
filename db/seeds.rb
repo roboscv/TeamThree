9c7bface84fc8bce850dabd84ae5ee48a1ca30ec
Adam Abdulhamid <aaa7vc@virginia.edu>
2014-10-09 20:51:15 -0400

    Added seed data for Author model and updated view in Authors index to show all authors in an ordered list.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Author.create(name:"Tolkien", dob:19350412, nationality:"American", awards:"Awards...", biography:"He wrote the lord of the rings series. This DOB is so wrong...", image_url:"tolkien.jpg")
+Author.create(name:"Rand", dob:18191212, nationality:"German", awards:"Awards...", biography:"She wrote something important too. DOB is also wrong...", image_url:"rand.jpg")
+Author.create(name:"Card", dob:17890913, nationality:"American", awards:"Awards...", biography:"He wrote something about something that people cared about. DOB so, so wrong...", image_url:"card.jpg")
+Author.create(name:"Orwell", dob:18010222, nationality:"Russian", awards:"Awards...", biography:"He wrote about depressing stuff that a lot of people probably cared about. DOB...lol", image_url:"orwell.jpg")
+Author.create(name:"Martin", dob:19650730, nationality:"Westeroc", awards:"Highborn", biography:"He's the man. Hands down the GOAT. DOB is definitely wrong but that don't matter.", image_url:"martin.gif")
