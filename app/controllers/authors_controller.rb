fc8989442572b694ff9ae5400ac5e3e35111b8fa
Adam Abdulhamid <aaa7vc@virginia.edu>
2014-10-16 20:52:00 -0400

    finished new/edit and destroy - also implemented error reporting on page and flas for successful creation of author - also started applying layouts (header, footer, sidebar) but nothing exits in those yet

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-
+	before_action :set_author, only: [ :edit, :show, :update, :destroy ]
 
 
 	def index
@@ -10,12 +10,11 @@ class AuthorsController < ApplicationController
 	def create
 		@author = Author.new(author_params)
 		if @author.save
-			redirect_to @author
+			redirect_to @author, notice: "#{@author.name} was created!"
 		else 
 			render :new
 		end
 	end
-
 
 	def new
 		@author = Author.new
@@ -23,19 +22,16 @@ class AuthorsController < ApplicationController
 
 
 	def edit
-		@author = Author.find(params[:id])
 	end
 
 
 	def show
-		@author = Author.find(params[:id])
 	end
 
 
 	def update
-		@author = Author.find(params[:id])
 		if @author.update(author_params)
-			redirect_to @author
+			redirect_to @author, notice: "#{@author.name} was updated!"
 		else 
 			render :new
 		end
@@ -43,11 +39,16 @@ class AuthorsController < ApplicationController
 
 
 	def destroy
-
+  		@author.destroy
+  		redirect_to authors_url
 	end
 
 private
 	def author_params
 		params.require(:author).permit(:name, :dob, :nationality, :awards, :biography, :image_url)
+	end
+
+	def set_author
+		@author = Author.find(params[:id])
 	end
 end
